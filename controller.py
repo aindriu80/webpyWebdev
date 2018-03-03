80e978eae0fb50e27e59445d676df8278246f291
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-03-03 10:31:07 +0000

    Sessions - initial

--- controller.py
+++ controller.py
@@ -1,17 +1,25 @@
 import web
 from Models import RegisterModel, LoginModel
 
+web.config.debug = False
+
 urls = (
     '/', 'Home',
     '/register', 'Register',
     '/login', 'Login',
+    '/logout', "Logout",
     '/postregistration', 'PostRegistration',
     '/check-login', 'Checklogin'
 
 )
 
-render = web.template.render("views/templates", base="MainLayout")
+
 app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={'user': 'none'})
+session_data = session._initializer
+
+render = web.template.render("views/Templates", base="MainLayout", globals={'session': session_data, 'current_user': session_data["user"]})
+
 
 
 # Classes/Routes
@@ -51,6 +59,12 @@ class Checklogin:
             return isCorrect
 
         return "error"
+
+
+class Logout:
+    def GET(self):
+        session.kill()
+        return "success"
 
 if __name__ == "__main__":
     app.run()
